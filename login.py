ca9c7856fe0c727f65cfe1778f544c13f4572986
marcoshaa <m.andrade.assis@gmail.com>
2021-10-18 23:55:18 -0300

    att login

--- login.py
+++ login.py
@@ -1,4 +1,25 @@
-from PyQt5 import uic,QtWidgets
+from PyQt5 import  uic,QtWidgets
 
 
-def 
+def view_second_screen():
+    FristScreen.label_4.setText("")
+    user_name = FristScreen.user_name.text()
+    password = FristScreen.password.text()
+    if user_name == "ADM" and password == "adm" :
+        FristScreen.close()
+        SecondScreen.show()
+
+    else :
+        FristScreen.label_4.setText("Dados invalidos!")
+    
+def logout():
+    SecondScreen.close()
+    FristScreen.show()
+
+
+app=QtWidgets.QApplication([])
+FristScreen = uic.loadUi("FristScreen.ui")
+SecondScreen = uic.loadUi("Second_Screen.ui")
+
+FristScreen.show()
+app.exec()
