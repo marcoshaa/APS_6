68fd12050badc67e538eefecdd26b9cfcc8ad46c
marcoshaa <m.andrade.assis@gmail.com>
2021-10-19 22:53:30 -0300

    add nivel

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 from PyQt5 import  uic,QtWidgets
-
+import sqlite3
 
 def view_second_screen():
     FristScreen.label_4.setText("")
@@ -20,6 +20,10 @@ def logout():
 app=QtWidgets.QApplication([])
 FristScreen = uic.loadUi("FristScreen.ui")
 SecondScreen = uic.loadUi("Second_Screen.ui")
+FristScreen.enter_Button.clicked.connect(view_second_screen)
+SecondScreen.buttonSairT2.clicked.connect(logout)
+FristScreen.password.setEchoMode(QtWidgets.QLineEdit.Password)
+
 
 FristScreen.show()
 app.exec()
