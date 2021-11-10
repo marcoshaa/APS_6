308e3b336bba5c634e46305ccb4903bb35cbc686
marcoshaa <m.andrade.assis@gmail.com>
2021-11-10 00:42:37 -0300

    img nivel3

--- .qt_for_python/uic/level.py
+++ .qt_for_python/uic/level.py
@@ -14,46 +14,63 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_level_1(object):
     def setupUi(self, level_1):
         level_1.setObjectName("level_1")
-        level_1.resize(699, 725)
-        level_1.setStyleSheet("background-color: rgb(44, 128, 255);")
+        level_1.resize(698, 708)
+        level_1.setStyleSheet("")
         self.centralwidget = QtWidgets.QWidget(level_1)
         self.centralwidget.setObjectName("centralwidget")
         self.intro_level1 = QtWidgets.QLabel(self.centralwidget)
-        self.intro_level1.setGeometry(QtCore.QRect(130, 0, 321, 31))
+        self.intro_level1.setGeometry(QtCore.QRect(180, 0, 321, 41))
         font = QtGui.QFont()
         font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
         self.intro_level1.setFont(font)
         self.intro_level1.setObjectName("intro_level1")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 60, 351, 41))
+        self.label.setGeometry(QtCore.QRect(120, 40, 481, 41))
+        self.label.setMinimumSize(QtCore.QSize(351, 0))
         font = QtGui.QFont()
         font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(490, 620, 141, 31))
-        self.pushButton.setStyleSheet("background-color: rgb(60, 60, 179);\n"
+        self.pushButton.setGeometry(QtCore.QRect(480, 570, 141, 61))
+        self.pushButton.setStyleSheet("background-color: rgb(0, 102, 0);\n"
+"alternate-background-color: rgb(0, 85, 0);\n"
 "border-style: outset;\n"
 "border-width: 2px;\n"
 "border-radius: 15px;\n"
 "border-color: black;")
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(130, 610, 141, 31))
-        self.pushButton_2.setStyleSheet("background-color: rgb(60, 60, 179);\n"
+        self.pushButton_2.setGeometry(QtCore.QRect(130, 560, 141, 71))
+        self.pushButton_2.setStyleSheet("background-color: rgb(0, 102, 0);\n"
+"alternate-background-color: rgb(0, 85, 0);\n"
 "border-style: outset;\n"
 "border-width: 2px;\n"
 "border-radius: 15px;\n"
 "border-color: black;")
         self.pushButton_2.setObjectName("pushButton_2")
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(40, 110, 611, 381))
+        self.textEdit.setGeometry(QtCore.QRect(50, 120, 611, 381))
+        self.textEdit.setStyleSheet("")
         self.textEdit.setObjectName("textEdit")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(-10, 0, 711, 691))
+        self.label_2.setStyleSheet("")
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap(":/img/soja.jfif"))
+        self.label_2.setScaledContents(True)
+        self.label_2.setObjectName("label_2")
+        self.label_2.raise_()
+        self.intro_level1.raise_()
+        self.label.raise_()
+        self.pushButton.raise_()
+        self.pushButton_2.raise_()
+        self.textEdit.raise_()
         level_1.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(level_1)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 699, 21))
-        self.menubar.setObjectName("menubar")
-        level_1.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(level_1)
         self.statusbar.setObjectName("statusbar")
         level_1.setStatusBar(self.statusbar)
@@ -71,8 +88,15 @@ class Ui_level_1(object):
         self.textEdit.setHtml(_translate("level_1", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">    As atividades do grupo se inicou em São Miguel do Iguaçú (PR). Sua renda baseava-se em produção de soja, sementes e na comercialização de safras. Em 1979 com a aquisição de terras no Mato Grosso possibilitou a expansão de suas produçoes de soja e implementação do algodão e do milho a safras.</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">    Após a aquisição, o grupo mudo sua sede para Rondonópolis (MT)</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt;\"><br /></p></body></html>"))
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">    As atividades do grupo se inicou em São Miguel do Iguaçú (PR). Sua renda baseava-se em produção de soja, sementes e na comercialização de safras. Em 1979 com a aquisição de terras no Mato Grosso possibilitou a expansão de suas produçoes de soja e implementação do algodão e do milho a safras.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">    Após a aquisição, o grupo mudo sua sede para Rondonópolis (MT), onde foi realizando aquisições de terras e aumentando o numero de fazendas. Atualmente o grupo conta com 10 fazendas apenas no territorio do Mato Grosso, estando nos municipios de; Sapezal, Campo Novo Dos Parecis, Querência, Itiquira e Rondonópolis.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">    O grupo vem aumentando gradualmente o volume de suas safras. A nas safras 2016/2017, a Soja em uma área de 163 mil hectares produziu 569 toneladas; Semente de Soja em uma área de 10,20 mil hectares produziu 20,36 toneladas; Milho em uma área  de 63,24 mil hectares porduziu 357,48 toneladas; Algodão em uma área de 52,69 mil hectares produziu 233,17 toneladas.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">    A empresa atualmente fornece para o mercado interno e externo, tendo frota de caminhões para o abastecimento interno, e possuindo rotas maritimas para exportação. A empresa AMAGGI na divisão agro são mais de 3.000 funcionarios entre produção e na comercialização.</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">    </span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">    Com a demanda da expanção houve um aumento na frota dos maquinarios, na divisão agro possuem 30 colheitadeiras axial, 15 tratores, 15 plantadeiras de 24 linhase mais de 21 autopropelidos.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p></body></html>"))
+import img_rc
--- .qt_for_python/uic/level3.py
+++ .qt_for_python/uic/level3.py
@@ -14,19 +14,53 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_level_3(object):
     def setupUi(self, level_3):
         level_3.setObjectName("level_3")
-        level_3.resize(800, 596)
-        level_3.setStyleSheet("background-color: rgb(0, 85, 255);")
+        level_3.resize(800, 594)
+        level_3.setStyleSheet("")
         self.centralwidget = QtWidgets.QWidget(level_3)
         self.centralwidget.setObjectName("centralwidget")
         self.intro_Level3 = QtWidgets.QLabel(self.centralwidget)
-        self.intro_Level3.setGeometry(QtCore.QRect(230, 100, 221, 101))
+        self.intro_Level3.setGeometry(QtCore.QRect(0, -10, 801, 571))
         font = QtGui.QFont()
         font.setPointSize(50)
         self.intro_Level3.setFont(font)
+        self.intro_Level3.setStyleSheet("\n"
+"")
+        self.intro_Level3.setText("")
+        self.intro_Level3.setPixmap(QtGui.QPixmap(":/nivel3/armMaggi.jpg"))
+        self.intro_Level3.setScaledContents(True)
         self.intro_Level3.setObjectName("intro_Level3")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(250, 10, 201, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        font.setKerning(True)
+        self.label.setFont(font)
+        self.label.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-radius: 15px;\n"
+"border-color: black;")
+        self.label.setObjectName("label")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(20, 110, 381, 401))
+        self.textBrowser.setObjectName("textBrowser")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(530, 440, 141, 61))
+        self.pushButton_4.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"alternate-background-color: rgb(0, 85, 0);\n"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-radius: 15px;\n"
+"border-color: black;")
+        self.pushButton_4.setObjectName("pushButton_4")
         level_3.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(level_3)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
         self.menubar.setObjectName("menubar")
         level_3.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(level_3)
@@ -39,4 +73,6 @@ class Ui_level_3(object):
     def retranslateUi(self, level_3):
         _translate = QtCore.QCoreApplication.translate
         level_3.setWindowTitle(_translate("level_3", "MainWindow"))
-        self.intro_Level3.setText(_translate("level_3", "nivel 3"))
+        self.label.setText(_translate("level_3", "Agrotóxico"))
+        self.pushButton_4.setText(_translate("level_3", "PushButton"))
+import imgNivel3_rc
