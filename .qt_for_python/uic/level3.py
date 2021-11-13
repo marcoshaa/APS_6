620db4e45a3306a0c722f9c47dc770cbf14fdda2
marcoshaa <m.andrade.assis@gmail.com>
2021-11-12 23:46:36 -0300

    info L2

--- .qt_for_python/uic/level3.py
+++ .qt_for_python/uic/level3.py
@@ -46,10 +46,16 @@ class Ui_level_3(object):
 "border-color: black;")
         self.label.setObjectName("label")
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(20, 110, 421, 401))
+        self.textBrowser.setGeometry(QtCore.QRect(20, 110, 471, 401))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.textBrowser.setFont(font)
         self.textBrowser.setObjectName("textBrowser")
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(530, 440, 141, 61))
+        self.pushButton_4.setGeometry(QtCore.QRect(610, 390, 141, 61))
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.pushButton_4.setFont(font)
         self.pushButton_4.setStyleSheet("background-color: rgb(255, 255, 255);\n"
 "alternate-background-color: rgb(0, 85, 0);\n"
 "border-style: outset;\n"
@@ -76,7 +82,25 @@ class Ui_level_3(object):
         self.textBrowser.setHtml(_translate("level_3", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">    O grupo AMAGGI não faz apenas o consumo de agrotóximos como o Glisofato; eles realizam a venda de produtos para reabiliar o solo e manutenção da plantação. </span></p></body></html>"))
-        self.pushButton_4.setText(_translate("level_3", "PushButton"))
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:12pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">    O grupo AMAGGI não faz apenas o consumo de agrotóximos como o Glisofato; eles realizam a venda de produtos para reabiliar o solo e manutenção da plantação. </p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Prdutos Quimicos utilizados:<br /><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636; background-color:#ffffff;\">– Hexano</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Poliuretano</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Glifosfato</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Aflatoxina</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Ácido Clorídrico</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Álcool Etílico</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Solução Acetônica</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Ácido Sulfúrico</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Eter Petróleo</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Hidróxido Sódio</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– NO2 Amônia</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Ácido Sulfonico</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Hipoclorito Sódio</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Álcool Metílico</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Cloreto Trifeniltetra</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Hidróxido Sódio</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:150%; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu\',\'Helvetica\',\'Arial\',\'sans-serif\'; font-weight:296; color:#363636;\">– Iodeto Potássio</span></p></body></html>"))
+        self.pushButton_4.setText(_translate("level_3", "SAIR"))
 import imgNivel3_rc
